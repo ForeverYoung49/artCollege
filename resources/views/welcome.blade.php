22e7ff8427df5e4475e60c655ff7d88d0449fbbd
ForeverYoung <ForeverYoung49@users.noreply.github.com>
2021-03-11 18:30:14 +0500

    321

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,9 @@
                 @yield('content')
             </div>
 
-        
+        <div>
         <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
         <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+        </div>
     </body>
 </html>
