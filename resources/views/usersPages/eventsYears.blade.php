e3db5e5e5fd1b0a733d70269b3c0e07a2bb89453
ForeverYoung <ForeverYoung49@users.noreply.github.com>
2021-03-11 18:25:26 +0500

    321

--- resources/views/usersPages/eventsYears.blade.php
+++ resources/views/usersPages/eventsYears.blade.php
@@ -11,8 +11,7 @@
         </div>
         <div class="row projects">
             @foreach($events as $e)
-            <a href="/events/{{ $e->year }}" class="col-sm-6 col-lg-4 item">
-            <div class=" years">
+            <a href="/events/{{ $e->year }}" class="col-sm-6 col-lg-4 item years">
                 <h3 class="name">
                     {{ $e->year }}
                 </h3>
@@ -20,7 +19,6 @@
                 <p style="padding: 10px 30px;">
                     {!! html_entity_decode(nl2br(e(mb_strimwidth($e->description, 0, 80)))) !!}...
                 </p>
-                </div>
             </a>
             @endforeach
         </div>
