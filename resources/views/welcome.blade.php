89a3a7d86054e2bde4949f7984e7f20208b2d0aa
ForeverYoung <ForeverYoung49@users.noreply.github.com>
2021-03-11 18:15:00 +0500

    123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title','')</title>
         <script src="https://cdn.ckeditor.com/ckeditor5/11.0.1/classic/ckeditor.js"></script>
+        <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome-all.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/fonts/ionicons.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/Article-Clean.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/Footer-Basic.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/Projects-Clean.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/sidebar-1.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/SIdebar-Responsive-2-1.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/SIdebar-Responsive-2.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/sidebar.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
     </head>
     <body class="antialiased">
         @include('fnh.header')
