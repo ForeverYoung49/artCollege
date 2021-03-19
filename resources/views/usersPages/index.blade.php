da81d194501d9bd5c9fb816817695da2d13349ed
ForeverYoung <ForeverYoung49@users.noreply.github.com>
2021-03-19 20:13:19 +0500

    123

--- resources/views/usersPages/index.blade.php
+++ resources/views/usersPages/index.blade.php
@@ -10,36 +10,36 @@
             <p class="text-center">Описание категории</p>
         </div> -->
         <div class="row projects">
-            <div class="col-sm-6 col-lg-4 item">
+            <a href="/graduates" class="col-sm-6 col-lg-4 item">
                 <img class="img-fluid" src="assets/img/desk.jpg">
                 <h3 class="name">Выпускники</h3>
                 <p class="description">Выпускники колледжа искусств</p>
-            </div>
-            <div class="col-sm-6 col-lg-4 item">
+            </a>
+            <a href="/teachers" class="col-sm-6 col-lg-4 item">
                 <img class="img-fluid" src="assets/img/building.jpg">
                 <h3 class="name">Почетные преподаватели</h3>
                 <p class="description">Педагоги колледжа искусств<br><br></p>
-            </div>
-            <div class="col-sm-6 col-lg-4 item">
+            </a>
+            <a href="/events" class="col-sm-6 col-lg-4 item">
                 <img class="img-fluid" src="assets/img/loft.jpg">
                 <h3 class="name">Важные события</h3>
                 <p class="description">События колледжа искусств<br><br></p>
-            </div>
-            <div class="col-sm-6 col-lg-4 item">
+            </a>
+            <a href="/directors" class="col-sm-6 col-lg-4 item">
                 <img class="img-fluid" src="assets/img/minibus.jpeg">
                 <h3 class="name">Директора</h3>
                 <p class="description">Директора колледжа искусств<br><br></p>
-            </div>
-            <div class="col-sm-6 col-lg-4 item">
+            </a>
+            <a href="/videos" class="col-sm-6 col-lg-4 item">
                 <img class="img-fluid" src="assets/img/minibus.jpeg">
                 <h3 class="name">Видеоматериалы</h3>
                 <p class="description">Видеоархив<br><br></p>
-            </div>
-            <div class="col-sm-6 col-lg-4 item">
+            </a>
+            <a href="/honored_workers" class="col-sm-6 col-lg-4 item">
                 <img class="img-fluid" src="assets/img/minibus.jpeg">
                 <h3 class="name">Заслуженные работники культуры</h3>
                 <p class="description">Работники культуры колледжа искусств<br><br></p>
-            </div>
+            </a>
         </div>
     </div>
 </div>
